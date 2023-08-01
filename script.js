bc6b8767935d22be7f174532c1a0f6f8889d8784
Raimundas Radavicius <raimundas_radavicius@yahoo.com>
2023-08-01 13:24:58 +0100

    tabbed components

--- script.js
+++ script.js
@@ -8,7 +8,14 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 const btnScrollTo = document.querySelector(".btn--scroll-to");
+
 const section1 = document.querySelector("#section--1");
+
+const nav = document.querySelector(".nav");
+
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
 
 ///////////////////////////////////////
 // Modal window
@@ -67,3 +74,34 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
 });
+
+///////////////////////////////////////
+// Tabbed component
+
+// Event delegation
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
+  // console.log(clicked);
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active tabs
+  tabs.forEach((t) => t.classList.remove("operations__tab--active"));
+
+  // Active tab
+  clicked.classList.add("operations__tab--active");
+
+  console.log(clicked.dataset.tab);
+  // Remove active content
+  tabsContent.forEach((c) => c.classList.remove("operations__content--active"));
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
+});
+
+///////////////////////////////////////
+// Menu fade animation
+
+nav;
