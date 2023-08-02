a7f064c94bb0947b86a7ef17883e144d0fecddc6
Raimundas Radavicius <raimundas_radavicius@yahoo.com>
2023-08-02 08:53:01 +0100

    scroll event

--- script.js
+++ script.js
@@ -128,3 +128,19 @@ const handleHover = function (e) {
 nav.addEventListener("mouseover", handleHover.bind(0.5));
 
 nav.addEventListener("mouseout", handleHover.bind(1));
+
+///////////////////////////////////////
+// Sticky navigation
+
+/////////
+// Not the best way to make sticky navbar
+const initialCoords = section1.getBoundingClientRect();
+console.log(initialCoords);
+
+window.addEventListener("scroll", function () {
+  // console.log(window.scrollY);
+  if (window.scrollY > initialCoords.top) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+});
+/////////
+/////////
